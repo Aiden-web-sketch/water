87847c36dca7ad917ba517b78aa95953dc71a8cf
Aiden-web-sketch <72841441+Aiden-web-sketch@users.noreply.github.com>
2021-01-07 17:20:50 -0500

    Add files via upload

---
+++ unbrella.js
@@ -0,0 +1,22 @@
+class Particle {
+    constructor(x, y,radius) {
+ 
+     var options={
+         restituion:0.4
+     }
+
+      this.body=Bodies.circle(x,y,radius,options)
+      World.add(world, this.body);
+this.radius=radius
+this.color=color("black")
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      fill(this.color);
+      ellipseMode(RADIUS);
+      ellipse(0, 0,this.radius,this.radius);
+      pop();
+    }
+  };
